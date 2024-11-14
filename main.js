fb18ed2c203427b395d4073556fcbba85bd1201a
Griffon <55240335+RomainC17@users.noreply.github.com>
2024-11-14 14:36:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    fullscreen: true,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile('jeu/index.html');
+
+  // Écouter l'événement "close-app" pour fermer l'application
+  ipcMain.on("close-app", () => {
+    win.close();
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
