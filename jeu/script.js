a70cea7b2f68105ada4601c1c32114783bf20225
RomainC17 <romaincartier@laposte.net>
2024-11-20 00:22:19 +0100

    Externalisation des fonctions liées au son

--- jeu/script.js
+++ jeu/script.js
@@ -38,17 +38,6 @@ const tooltip = document.createElement("div");
 tooltip.classList.add("tooltip");
 document.body.appendChild(tooltip);
 
-const backgroundMusic = new Audio('sons/Fabrice-Tonnellier.mp3');
-backgroundMusic.volume = 0.2; // Volume réduit pour ne pas gêner
-backgroundMusic.loop = true; // Boucle
-backgroundMusic.play();
-
-// Gestionnaire de volume
-const volumeSlider = document.getElementById('volume-slider');
-volumeSlider.addEventListener('input', (event) => {
-  backgroundMusic.volume = event.target.value; // Ajuste le volume en fonction de la position du curseur
-});
-
 
 // Mise à jour du score affiché
 function updateScore() {
